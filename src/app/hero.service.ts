60e2de363927476f7d16d8a2633ba62ea7b4f843
Omolayo Victor <vomolayo@seamfix.com>
2019-09-25 08:38:56 +0100

    Add message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,10 +3,12 @@ import { Observable, of } from 'rxjs';
 
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
   constructor() {}
 
